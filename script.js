9e877029d347b277d599a532056b5948ced68202
Samriddhi Shreyasee <112820579+Samiriddhi@users.noreply.github.com>
2024-09-17 00:11:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstWord(s) {
   // your code here
+	let arr = s.split(" ");
+	if(arr.length == 0){
+		return " " ;
+	}else{
+		return arr[0];
+	}
 }
 
-// Do not change the code below
+Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
